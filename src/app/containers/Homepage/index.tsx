6943ff2f4041cfb24feeb4cc7cc3bf6f2dee7c36
NwaguChima <nwaguchima48@gmail.com>
2022-10-05 17:57:29 +0100

    added styles to page container

--- src/app/containers/Homepage/index.tsx
+++ src/app/containers/Homepage/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import tw from "twin.macro";
 
-const PageContainer = styled.div``;
+const PageContainer = styled.div`
+  ${tw`
+    flex
+    flex-col
+    w-full
+    h-full
+    items-center
+    overflow-x-hidden
+`}
+`;
 
 export function HomePage() {
   return (

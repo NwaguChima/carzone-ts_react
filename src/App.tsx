96c84d7dc94c15b47f7ae55ee24412f34779c6ca
NwaguChima <nwaguchima48@gmail.com>
2022-10-05 17:36:34 +0100

    completed tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import "./App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return <h1 className="text-3xl font-sm underline">Hello world!</h1>;
 }
 
 export default App;

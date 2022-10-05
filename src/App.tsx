78d78828e780fbc20b9fb1d150703f912dce6ac4
NwaguChima <nwaguchima48@gmail.com>
2022-10-05 18:00:57 +0100

    setup app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { HomePage } from "./app/containers/Homepage";
+
+const AppContainer = styled.div`
+  ${tw`
+  w-full
+  h-full
+  flex
+  flex-col
+  `}
+`;
 
 function App() {
-  return <h1 className="text-3xl font-sm underline">Hello world!</h1>;
+  return (
+    <AppContainer>
+      <HomePage />
+    </AppContainer>
+  );
 }
 
 export default App;

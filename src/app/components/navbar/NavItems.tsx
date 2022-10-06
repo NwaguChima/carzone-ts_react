fe47b19b634b258362c48327c4abde2371e9e7c7
NwaguChima <nwaguchima48@gmail.com>
2022-10-06 03:53:56 +0100

    completed menu implementation

--- src/app/components/navbar/NavItems.tsx
+++ src/app/components/navbar/NavItems.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import tw from "twin.macro";
 
 import { slide as Menu } from "react-burger-menu";
@@ -9,48 +9,61 @@ import menuStyles from "./menuStyles";
 
 const ListContainer = styled.ul`
   ${tw`
-        flex
-        list-none
-    `}
+    flex
+    flex-col
+    list-none
+  `};
 `;
 
-const NavItem = styled.li`
+const NavItem = styled.li<{ menu?: any }>`
   ${tw`
-        text-xs
-        md:text-base
-        text-black
-        font-medium
-        mr-1
-        md:mr-5
-        cursor-pointer
-        transition
-        duration-300
-        ease-in-out
-        hover:text-gray-700
-  `}
+    text-sm
+    md:text-base
+    text-black
+    font-medium
+    mr-1
+    md:mr-5
+    cursor-pointer
+    transition
+    duration-300
+    ease-in-out
+    hover:text-gray-700
+  `};
+
+  ${({ menu }) =>
+    menu &&
+    css`
+      ${tw`
+      text-white
+      text-xl
+      mb-3
+      focus:text-white
+    `};
+    `};
 `;
 
 export function NavItems() {
   const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
 
-  if (isMobile) {
+  if (isMobile)
     return (
       <Menu right styles={menuStyles}>
-        <NavItem>
-          <a href="#">Home</a>
-        </NavItem>
-        <NavItem>
-          <a href="#">Cars</a>
-        </NavItem>
-        <NavItem>
-          <a href="#">Services</a>
-        </NavItem>
-        <NavItem>
-          <a href="#">Contact Us</a>
-        </NavItem>
+        <ListContainer>
+          <NavItem menu>
+            <a href="#">Home</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Cars</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Services</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Contact Us</a>
+          </NavItem>
+        </ListContainer>
       </Menu>
     );
-  }
 
   return (
     <ListContainer>
